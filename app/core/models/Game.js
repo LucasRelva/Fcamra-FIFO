d0a74080c4765e8377f8d026f5d6cbff377a5879
Lucas Relva <lucas-1880@outlook.com>
2020-11-05 21:24:55 -0300

    Models

---
+++ app/core/models/Game.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Game extends Model{
+    static init(sequelize){
+        super.init({
+            name: DataTypes.STRING,
+            slug: DataTypes.STRING,
+            is_active: DataTypes.BOOLEAN,
+        }, {
+            sequelize,
+            tableName: 'games',
+        })
+    }
+
+    static associate(models) {
+        this.belongsToMany(models.Unity, { foreignKey: 'game_id', through: 'unity_games', as: 'unities' })
+    }
+}
+
+module.exports = Game
