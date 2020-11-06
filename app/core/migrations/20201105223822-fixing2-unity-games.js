20bab570bed27f80b6253ac252afdcc60ee7f284
Lucas Relva <lucas-1880@outlook.com>
2020-11-05 21:23:58 -0300

    Migrations

---
+++ app/core/migrations/20201105223822-fixing2-unity-games.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.addColumn('unity_games', 'is_active', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
+      after: 'id'
+    },
+
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.removeColumn('unity_games', 'is_active');
+    
+  }
+};
