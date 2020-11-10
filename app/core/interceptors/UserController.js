523054c10fd730d06394ecf7afefd292a82ac421
Lucas Relva <lucas-1880@outlook.com>
2020-11-10 19:41:33 -0300

    Pequeno erro de syntax

--- app/core/interceptors/UserController.js
+++ app/core/interceptors/UserController.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 module.exports = {
     async list(req, res) {
         const users = await User.findAll();
-        if(!user) res.status(404).json({error:'error',msg: 'users not found!'})
+        if(!users) res.status(404).json({error:'error',msg: 'users not found!'})
         return res.json(users)
     },
 
