e6946849f8114828076f4321f5c202292617b7a1
Lucas Relva <lucas-1880@outlook.com>
2020-11-10 16:38:47 -0300

    Correção

--- app/core/models/Game.js
+++ app/core/models/Game.js
@@ -14,7 +14,7 @@ class Game extends Model{
 
     static associate(models) {
         this.belongsToMany(models.Unity, { foreignKey: 'game_id', through: 'unity_games', as: 'unities' })
-        this.hasMany(models.Match, { as: 'matchs' })
+        this.hasMany(models.Match, {foreignKey: 'game_id', as: 'matchs' })
     }
 }
 
