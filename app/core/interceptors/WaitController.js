47de7fec4a6bdfeefa9a8724d6a843e065325375
Lucas Relva <lucas-1880@outlook.com>
2020-11-20 21:31:01 -0300

    Conflito

--- app/core/interceptors/WaitController.js
+++ app/core/interceptors/WaitController.js
@@ -32,6 +32,7 @@ module.exports = {
 
         const waits = await Wait.findAll({
             attributes: ['status', 'id'],
+                // TODO colocar status diferente de 2 ou somente os que forem 0, e crriar um match controller q vai retornar os que foram stautus 1
             where: {
                 game_id
                 // TODO colocar status diferente de 2 ou somente os que forem 0, e crriar um match controller q vai retornar os que foram stautus 1
